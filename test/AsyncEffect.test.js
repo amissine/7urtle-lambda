0db9b333c824a130cd095a9215d90fc17c8c819a
Alec Missine <alec_missine@yahoo.com>
2021-08-03 11:23:15 -0600

    Use function feed

--- test/AsyncEffect.test.js
+++ test/AsyncEffect.test.js
@@ -1,7 +1,7 @@
 const λ = require('..')
 const assert = require('assert')
 const data = require('./data.json')
-/*
+
 for (const item of data) {
   const id = item[0], qt = item[item.length - 2], pt = item[item.length - 1]
   let delta  = qt - pt
@@ -11,34 +11,37 @@ for (const item of data) {
   }
   λ.log(`- ${id}: ${qt} >>>  ∆ ${delta}`)
 }
-λ.log('- sync feed DONE\n')
-*/
+λ.log('\n- sync feed DONE\n')
+
 mockAsyncFeed(0)
 
 function mockAsyncFeed (index, qt, pt) {
   let item = data[index]
   const id = item[0]
   if (index == 0) {
-    qt = item[item.length - 2]
-    λ.log(`- ${id}: ${qt}`)
-    item = data[1]
-    qt = item[item.length - 2]
-    pt = item[item.length - 1]
+    feed(item)
+    item = data[1]; qt = item[item.length - 2]; pt = item[item.length - 1]
     setTimeout(() => mockAsyncFeed(1, qt, pt), qt - pt)
     return;
   }
-
-  let delta  = qt - pt
-  if (delta > 1500) {
-    λ.log('-')
-  }
-  λ.log(`- ${id}: ${qt} >>>  ∆ ${delta}`)
+  feed(item)
   if (++index > data.length - 1) {
     λ.log('\n- mockAsyncFeed DONE\n')
     return;
   }
-  item = data[index]
-  qt = item[item.length - 2]
-  pt = item[item.length - 1]
+  item = data[index]; qt = item[item.length - 2]; pt = item[item.length - 1]
   setTimeout(() => mockAsyncFeed(index, qt, pt), qt - pt)
+}
+
+function feed (item) {
+  const id = item[0], qt = item[item.length - 2], pt = item[item.length - 1]
+  if (pt == 0) {
+    λ.log(`${id}: ${qt}`)
+    return;
+  }
+  let delta  = qt - pt
+  if (delta > 1500) {
+    λ.log('')
+  }
+  λ.log(`${id}: ${qt} >>>  ∆ ${delta}`)
 }
