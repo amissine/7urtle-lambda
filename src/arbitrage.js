ceb6215850f98bfae6ca0e195b1b2b1605b0ee08
Alec Missine <alec_missine@yahoo.com>
2021-08-12 18:07:05 -0400

    Complete AO pseudocode

--- src/arbitrage.js
+++ src/arbitrage.js
@@ -10,7 +10,7 @@ export const feed = item => {
   let delta = qt - pt
   if (delta > 1500) {
     log('')
-    spreadsClear()
+    spreadsNew()
   }
   log(`${id}: ${qt} >>>  ∆ ${delta}`)
   spreadsAdd(item)
@@ -18,22 +18,59 @@ export const feed = item => {
 
 let spreads = [], spreadsMaxLength = 0
 
-function spreadsClear () {
+function spreadsNew () {
   if (spreads.length > 0) {
     if (spreadsMaxLength == 0) {        // assuming 1st pack of spreads
       spreadsMaxLength = spreads.length // comes in full
     }
-    arbitrage()
+    arbitrage() // also runs on incomplete packs of spreads
   }
 }
 
 function spreadsAdd (item) {
-  spreads.push([item[0], item[1][0], item[2][0]]) // id, maxbid, minask
+  spreads.push([item[0], item[1][0], item[2][0]]) // xid, maxbid, minask
   if (spreads.length == spreadsMaxLength) {
     arbitrage()
   }
 }
 
+/*
+ Arbitrage opportunities can be found on spreads from different exchanges, taken
+ within a short timeframe < 1000 ms. For two exchanges x0 and x1, represented by
+ their spreads
+
+   s0 = [x0, [maxBidPrice, maxBidAmount, …], [minAskPrice, minAskAmount, …]]
+
+ and
+
+   s1 = [x1, [maxBidPrice, maxBidAmount, …], [minAskPrice, minAskAmount, …]]
+
+ , AO can be defined as follows:
+
+   sTopBid = s0[1][0] > s1[1][0] ? s0 : s1
+   sBtmAsk = s0[2][0] < s1[2][0] ? s0 : s1
+
+   if (sTopBid[1][0] > sBtmAsk[2][0]) {
+
+     AO(s0, s1) = (sTopBid[1][0] - sBtmAsk[2][0])
+                  * Math.min(sTopBid[1][1], sBtmAsk[2][1])
+   }
+
+ For N ≥ 2 exchanges, represented by their spreads S = [s0, s1, …, sNm1], AO can be
+ defined as follows:
+
+   AO(N) = Math.max(...Array.from(
+                         Array.from(S, pairs), 
+                         p => p && AO(p[0], p[1]) ?? 0))
+ where 
+
+   const pairs = (x, i) => {
+     const x2pair = Array.from(S, (x2p, j) => j > i ? x2p : undefined)
+     return Array.from(
+              Array.from(S, (x2p, j) => j > i ? x2p : undefined),
+              x2p => x2p ? [x, x2p] : undefined)
+   }
+*/
 function arbitrage () {
   if (spreads.length < 2) {
     log('=== NO ARBITRAGE POSSIBLE ===')
